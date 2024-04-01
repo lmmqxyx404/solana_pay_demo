031dddd8b0a16f4291d1db9a3389cc7e12136c64
lmmqxyx <1368470804@qq.com>
2024-04-02 00:00:02 +0800

    add layouts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Layout from '../components/Layout'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <title>Cookies Inc</title>
+    <Layout>
+
+      <title>Cookies Inc</title>
+
+
+    </Layout>
+
   )
 }
 
