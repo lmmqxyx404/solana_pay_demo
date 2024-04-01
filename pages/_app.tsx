507edcb621e0b8bbd28885c3e3a8a28c231e5edc
lmmqxyx <1368470804@qq.com>
2024-04-01 23:57:59 +0800

    learn solana

---
+++ pages/_app.tsx
@@ -0,0 +1,10 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <title>Cookies Inc</title>
+  )
+}
+
+export default MyApp
