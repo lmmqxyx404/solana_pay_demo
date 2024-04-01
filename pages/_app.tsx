68c9369cc62d13ecdc136698f8b041073b411dd6
lmmqxyx <1368470804@qq.com>
2024-04-02 00:01:30 +0800

    add Head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
-
-      <title>Cookies Inc</title>
-
-
+      <Head>
+        <title>Cookies Inc</title>
+      </Head>
     </Layout>
-
   )
 }
 
