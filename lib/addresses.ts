44e930570b5d4e57863785802c12ddf1a891a4ce
lmmqxyx <1368470804@qq.com>
2024-04-08 23:56:48 +0800

    add the address

--- lib/addresses.ts
+++ lib/addresses.ts
@@ -3,5 +3,5 @@ import { PublicKey } from '@solana/web3.js'
 // Your shop wallet address
 // todo: change the following address
 export const shopAddress = new PublicKey(
-  'CJksdzJWHJWTTGGNQ7pnufoGPNhsxN96sqP3VshAzU3N'
+  '284poV7uiYo9pyAGQc6VFXHXGnNJ2z3LdRBF3h5ZB8Gu'
 )
