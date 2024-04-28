09f43a21b94843c253256c8a7ee826fd8ea9d13b
lmmqxyx <1368470804@qq.com>
2024-04-28 20:54:02 +0800

    add

--- scripts/test.mjs
+++ scripts/test.mjs
@@ -1,6 +1,15 @@
-
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base'
-import { Connection, clusterApiUrl } from '@solana/web3.js'
+import {
+  Connection,
+  Keypair,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  SystemProgram,
+  Transaction,
+  clusterApiUrl,
+  sendAndConfirmTransaction,
+} from '@solana/web3.js'
+import base58 from 'bs58'
 
 const network = WalletAdapterNetwork.Testnet
 const endpoint = clusterApiUrl(network)
@@ -9,6 +18,26 @@ console.log(connection)
 try {
   const { blockhash } = await connection.getLatestBlockhash('finalized')
   console.log(blockhash)
+
+  const senderSecretKey = base58.decode()
+  const senderKeypair = Keypair.fromSecretKey(senderSecretKey)
+  const receiverPublicKey = new PublicKey()
+
+  // 创建交易指令
+  const transaction = new Transaction().add(
+    SystemProgram.transfer({
+      fromPubkey: senderKeypair.publicKey,
+      toPubkey: receiverPublicKey,
+      lamports: 0.1 * LAMPORTS_PER_SOL, // 将 SOL 转换为 lamports
+    })
+  )
+
+  // 签署并发送交易
+
+  const signature = await sendAndConfirmTransaction(connection, transaction, [
+    senderKeypair,
+  ])
+  console.log(signature)
 } catch (e) {
   console.log(e)
 }
