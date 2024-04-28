7c0e5c8891b957d8322b6210a353a9a6ab8e1800
lmmqxyx <1368470804@qq.com>
2024-04-28 23:59:19 +0800

    add the airdrop and transfer function

--- scripts/test.mjs
+++ scripts/test.mjs
@@ -15,29 +15,54 @@ const network = WalletAdapterNetwork.Testnet
 const endpoint = clusterApiUrl(network)
 const connection = new Connection(endpoint)
 console.log(connection)
-try {
-  const { blockhash } = await connection.getLatestBlockhash('finalized')
-  console.log(blockhash)
 
-  const senderSecretKey = base58.decode()
-  const senderKeypair = Keypair.fromSecretKey(senderSecretKey)
-  const receiverPublicKey = new PublicKey()
-
+const transferSolana=async (senderKeypair, receiverPublicKey, amount)=>{
   // 创建交易指令
   const transaction = new Transaction().add(
     SystemProgram.transfer({
       fromPubkey: senderKeypair.publicKey,
       toPubkey: receiverPublicKey,
-      lamports: 0.1 * LAMPORTS_PER_SOL, // 将 SOL 转换为 lamports
+      lamports: amount * LAMPORTS_PER_SOL, // 将 SOL 转换为 lamports
     })
   )
 
   // 签署并发送交易
-
   const signature = await sendAndConfirmTransaction(connection, transaction, [
     senderKeypair,
   ])
-  console.log(signature)
+  return signature
+}
+
+// 定义一个异步函数来请求空投
+async function requestAirdrop(publicKey) {
+  console.log(`Requesting airdrop to wallet: ${publicKey}`);
+
+  // 请求空投
+  const airdropSignature = await connection.requestAirdrop(
+      publicKey,
+      LAMPORTS_PER_SOL // 1 SOL
+  );
+
+  // 确认交易
+  await connection.confirmTransaction(airdropSignature, 'confirmed');
+  console.log('Airdrop successful!');
+
+  // 获取并打印新的余额
+  const balance = await connection.getBalance(publicKey);
+  console.log(`Wallet balance: ${balance / LAMPORTS_PER_SOL} SOL`);
+}
+
+try {
+  const { blockhash } = await connection.getLatestBlockhash('finalized')
+  console.log(blockhash)
+
+  // const senderSecretKey = base58.decode()
+  const senderKeypair = Keypair.fromSecretKey(senderSecretKey)
+  // const publicKey = new PublicKey();
+  // await requestAirdrop(publicKey)
+  const res=await transferSolana(senderKeypair, publicKey, 0.01)
+  console.log(res);
 } catch (e) {
   console.log(e)
 }
+
